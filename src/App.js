c307f36023acd7e92985e3c701bd5d46fdf88328
Brett Cimbalik <bcimbali@gmail.com>
2018-08-07 21:48:51 -0500

    cleaned up app.js indents

--- src/App.js
+++ src/App.js
@@ -35,38 +35,38 @@ class App extends Component {
           </div>
         </ScrollableAnchor>
         <ScrollableAnchor id={'section2'}>
-        <div className='bg-color page container-fluid'> 
-          <div className="display-3 h-50 row align-items-center">
-            {/* <div className='bg-color col'>  */}
-              <img data-aos="fade-right"  className="border-dark img-resp mx-auto rounded-circle" alt="Brett" src="./images/Profile_Pic.jpg"></img>
-            {/* </div> */}
-          </div>
-          <div className="d-flex display-3 justify-content-center h-50 row text-center main-font">
-            <div data-aos="fade-left" className='about-font bg-color col'>
-              I'm a web developer and programmer based in Chicago, IL. <br></br><br></br>
-              I make web applications, usually with the MERN stack. I primarily use Javascript, but learning a new language or framework isn't a hassle. I have several years of programming experience and am rather comfortable with front-end or back-end development.
+          <div className='bg-color page container-fluid'> 
+            <div className="display-3 h-50 row align-items-center">
+              {/* <div className='bg-color col'>  */}
+                <img data-aos="fade-right"  className="border-dark img-resp mx-auto rounded-circle" alt="Brett" src="./images/Profile_Pic.jpg"></img>
+              {/* </div> */}
+            </div>
+            <div className="d-flex display-3 justify-content-center h-50 row text-center main-font">
+              <div data-aos="fade-left" className='about-font bg-color col'>
+                I'm a web developer and programmer based in Chicago, IL. <br></br><br></br>
+                I make web applications, usually with the MERN stack. I primarily use Javascript, but learning a new language or framework isn't a hassle. I have several years of programming experience and am rather comfortable with front-end or back-end development.
+              </div>
             </div>
           </div>
-        </div>
         </ScrollableAnchor>
         <ScrollableAnchor id={'section3'}>
-        <div className='bg-color page container-fluid'> 
-          <div className="d-flex display-3 justify-content-center h-100 row align-items-center text-center main-font">  
-            <div data-aos="fade-left" className='bg-color display-3 main-font'> 
-            Portfolio
+          <div className='bg-color page container-fluid'> 
+            <div className="d-flex display-3 justify-content-center h-100 row align-items-center text-center main-font">  
+              <div data-aos="fade-left" className='bg-color display-3 main-font'> 
+                Portfolio
+              </div>
             </div>
           </div>
-        </div>
         </ScrollableAnchor>
         <ScrollableAnchor id={'section4'}>
-        <div className='bg-color page container-fluid'> 
-          <div className="d-flex display-3 justify-content-center h-100 row align-items-center text-center main-font">  
-            <div data-aos="fade-right" className='about-font bg-color display-3'> 
-            Feel free to contact me at: <br></br><br></br>
-            hello [at] brettcimbalik [dot] com
+          <div className='bg-color page container-fluid'> 
+            <div className="d-flex display-3 justify-content-center h-100 row align-items-center text-center main-font">  
+              <div data-aos="fade-right" className='about-font bg-color display-3'> 
+              Feel free to contact me at: <br></br><br></br>
+              hello [at] brettcimbalik [dot] com
+              </div>
             </div>
           </div>
-        </div>
         </ScrollableAnchor>
         <Footer />
       </div>
