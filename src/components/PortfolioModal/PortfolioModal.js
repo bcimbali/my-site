150be49decb8b8c58629f76c32bde11650d75767
Brett Cimbalik <bcimbali@gmail.com>
2018-08-12 20:45:30 -0500

    removed borders in modal

--- src/components/PortfolioModal/PortfolioModal.js
+++ src/components/PortfolioModal/PortfolioModal.js
@@ -8,13 +8,13 @@ class PortfolioModal extends Component  {
     
     return (
         <Modal visible={this.props.isOpen} onClickBackdrop={this.props.toggle}>
-            <div className="bg-color justify-content-center modal-header mx-auto w-100">
+            <div className="bg-color border-bottom-0 justify-content-center modal-header mx-auto w-100">
                 <h4 className="font-weight-bold modal-title">{this.props.name}</h4>
             </div>
             <div className="bg-color modal-body">
                 <p>{this.props.description}</p>
             </div>
-            <div className="bg-color justify-content-between modal-footer">
+            <div className="bg-color border-top-0 justify-content-between modal-footer">
                 <a href={this.props.live} target="_blank" ><i class="fas fa-2x fa-desktop modal-icon"></i></a>
                 <a href={this.props.github} target="_blank" ><i class="fab fa-2x fa-github-alt modal-icon"></i></a>
                 <button className="bg-color modal-icon remove-bootstrap-glow" onClick={this.props.toggle}>
