ed85a05018877690fdcef18c270cfb8ba492df47
bcimbali <bcimbali@gmail.com>
2018-08-14 16:26:54 -0500

    fixed top nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component  {
     
     return (
 
-      <nav className="bg-color border-bottom border-dark d-flex navbar flex-nowrap justify-content-between navbar-expand-lg navbar-light sticky-top">
+      <nav className="bg-color border-bottom border-dark d-flex navbar flex-nowrap justify-content-between navbar-expand-lg navbar-light fixed-top">
         <a className="fade-in hvr-underline-from-center name-font navbar-brand font-weight-bold m-0 px-2" href="#section1">Brett Cimbalik</a>
 
           <div className="d-flex justify-content-center ml-auto nav-list-font">
