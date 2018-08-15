b101487f9cf82a3c538fe54e862ae7cd1249ca84
Brett Cimbalik <bcimbali@gmail.com>
2018-08-14 21:57:44 -0500

    portfolio card sizing

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -10,14 +10,14 @@ class PortfolioCard extends Component  {
     return (
 
         <div className="col-md-4">
-                <div data-aos="fade-left" className='border border-dark bg-color max-h mx-auto my-2 portfolio-card'> 
+                <div data-aos="fade-left" className='border border-dark bg-color max-h mx-auto my-2 portfolio-card rounded'> 
                   <img className="image img-fluid max-h-img" src={this.props.image}/>
                   <div className="border border-dark middle rounded">
                   <div className="text" onClick={() => {this.props.modalHandler(this.props.id - 1)}}>
                     Learn More
                   </div>
                   </div>
-                  <p>{this.props.name}</p>
+                  <p className="p-2 portfolio-card-font">{this.props.name}</p>
                 </div>
                 
                 {/* {console.log("props for PortfolioCard", this.props)} */}
