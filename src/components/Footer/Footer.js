65b14ca3b2e4546d933807326ac9927b0ab77656
Brett Cimbalik <bcimbali@gmail.com>
2018-08-29 22:33:32 -0500

    added resume to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,6 +12,7 @@ class Footer extends Component  {
         <footer className="footer d-flex justify-content-center">
             <a href="https://github.com/bcimbali" target="_blank"><i className="fab m-3 fa-3x fa-github-square footer-icon hvr-wobble-skew"></i></a>
             <a href="https://www.linkedin.com/in/brett-cimbalik/" target="_blank"><i className="fab m-3 fa-3x fa-linkedin footer-icon hvr-wobble-skew"></i></a>
+            <a href="https://drive.google.com/file/d/1bVxm7vubNbH8eS0qrormKvvfZagDKLo6/view?usp=sharing" target="_blank"><i className="fas fa-file-alt m-3 fa-3x footer-icon hvr-wobble-skew"></i></a>
         </footer>
         );
     };
