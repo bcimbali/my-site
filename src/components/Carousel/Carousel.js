7d269609b8d386d6d345f2849d6c9809bbdfe1b6
Brett Cimbalik <bcimbali@gmail.com>
2018-09-01 15:02:51 -0500

    refactored navbar and footer

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,40 +1,35 @@
-import React, {Component} from "react";
+import React from "react";
 
-
-
-class Carousel extends Component  {
-
-  render() {
+function Carousel(props)  {
     
-    return (
-        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-                <div className="carousel-img carousel-item active">
-                    <img className="carousel-img d-block w-100" src={this.props.imageOne} alt="First slide" />
-                </div>
-                <div className="carousel-img carousel-item">
-                    <img className="carousel-img d-block w-100" src={this.props.imageTwo} alt="Second slide" />
-                </div>
-                <div className="carousel-img carousel-item">
-                    <img className="carousel-img d-block w-100" src={this.props.imageThree} alt="Third slide" />
-                </div>
-            </div>
-            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only indicator-color">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
-        </div>
-      );
-    };
+  return (
+    <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+      <ol className="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div className="carousel-inner">
+          <div className="carousel-img carousel-item active">
+              <img className="carousel-img d-block w-100" src={props.imageOne} alt="First slide" />
+          </div>
+          <div className="carousel-img carousel-item">
+              <img className="carousel-img d-block w-100" src={props.imageTwo} alt="Second slide" />
+          </div>
+          <div className="carousel-img carousel-item">
+              <img className="carousel-img d-block w-100" src={props.imageThree} alt="Third slide" />
+          </div>
+      </div>
+      <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only indicator-color">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+      </a>
+    </div>
+    );
 };
 
 export default Carousel;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,13 @@
-import React, {Component} from "react";
+import React from "react";
 
-
-class Footer extends Component  {
-
-
-
-    render() {
-    
+function Footer() {
     return (
-
         <footer className="footer d-flex justify-content-center">
             <a href="https://github.com/bcimbali" target="_blank" rel="noopener noreferrer"><i className="fab m-3 fa-3x fa-github-square footer-icon hvr-wobble-skew"></i></a>
             <a href="https://www.linkedin.com/in/brett-cimbalik/" target="_blank" rel="noopener noreferrer"><i className="fab m-3 fa-3x fa-linkedin footer-icon hvr-wobble-skew"></i></a>
             <a href="https://drive.google.com/file/d/1bVxm7vubNbH8eS0qrormKvvfZagDKLo6/view?usp=sharing" target="_blank" rel="noopener noreferrer"><i className="fas fa-file-alt m-3 fa-3x footer-icon hvr-wobble-skew"></i></a>
         </footer>
-        );
-    };
+    );
 };
 
 export default Footer;
