bc414b7328a02bc262e50e0b34a36c5fef02b6e3
Brett Cimbalik <bcimbali@gmail.com>
2018-09-01 14:39:14 -0500

    portfoliocard is now functional component

--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -1,31 +1,20 @@
 import React, {Component} from "react";
 
-
-
-class PortfolioCard extends Component  {
-
-
-  render() {
-    
-    return (
-
-        <div className="col-md-4">
-                <div data-aos="fade-left" className='min-h max-w mx-auto my-3 portfolio-card rounded'> 
-                  <img className="image img-fluid max-h-img p-2 rounded" src={this.props.image} alt='portfolio piece'/>
-                  <div className="border border-dark middle rounded">
-                  <div className="text" onClick={() => {this.props.modalHandler(this.props.id - 1)}}>
-                    Check it Out
-                  </div>
-                  </div>
-                  <h3 className="p-2 portfolio-card-font">{this.props.name}</h3>
-                  <p className="p-2">{this.props.description}</p>
-                </div>
-                
-                {/* {console.log("props for PortfolioCard", this.props)} */}
-                {/* {console.log("this in PortflioCard", this)} */}
-        </div>
-      );
-    };
+function PortfolioCard(props)  { 
+  return (
+  <div className="col-md-4">
+      <div data-aos="fade-left" className='min-h max-w mx-auto my-3 portfolio-card rounded'> 
+        <img className="image img-fluid max-h-img p-2 rounded" src={props.image} alt='portfolio piece'/>
+          <div className="border border-dark middle rounded">
+            <div className="text" onClick={() => {props.modalHandler(props.id - 1)}}>
+              Check it Out
+            </div>
+          </div>
+        <h3 className="p-2 portfolio-card-font">{props.name}</h3>
+        <p className="p-2">{props.description}</p>
+      </div>
+  </div>
+  );
 };
 
 export default PortfolioCard;
