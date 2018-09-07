b7e4cefe358b80337446534e09491f9bb0aa1dd8
Brett Cimbalik <bcimbali@gmail.com>
2018-09-06 21:45:15 -0500

    h1 to my name so that page contains at least one level-one heading

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 function Navbar()  {    
   return (
     <nav className="bg-color border-bottom border-dark d-flex navbar flex-nowrap gray-font justify-content-between navbar-expand-lg navbar-light fixed-top">
-      <a className="fade-in hvr-underline-from-center name-font navbar-brand font-weight-bold m-0 px-2 gray-font" href="#section1">
-        Brett Cimbalik
-      </a>
+      <h1>
+        <a className="fade-in hvr-underline-from-center name-font navbar-brand font-weight-bold m-0 px-2 gray-font" href="#section1">
+          Brett Cimbalik
+        </a>
+      </h1>
       <div className="d-flex justify-content-center ml-auto nav-list-font">
         <a className="fade-in hvr-underline-from-center px-2 text-dark" href="#section2">About</a>
         <a className="fade-in hvr-underline-from-center px-2 text-dark" href="#section3">Portfolio</a>
