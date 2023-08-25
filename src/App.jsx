20f34c389cba42b1870aa76b1ff8058b46630787
Brett Cimbalik <bcimbali@gmail.com>
2023-08-24 19:54:33 -0500

    fix(styles): set maxWidth & layout fixes

--- src/App.jsx
+++ src/App.jsx
@@ -53,92 +53,95 @@ class App extends Component {
     const { details, selected_piece } = this.state;
     configureAnchors({ offset: -50, scrollDuration: 700 });
     return (
-      <div className="body">
-        <Navbar className="w-100" />
-        <ScrollableAnchor className="w-100" id={"section1"}>
-          <div className="bg-color page container-fluid">
-            <div className="d-flex display-3 justify-content-center h-75 row align-items-center text-center main-font">
-              <div className="col fade-in gray-font" data-testid="name-intro">
-                Hey, I'm Brett Cimbalik. I'm a front-end developer.
+      <>
+        <div className="content-container bg-color">
+          <Navbar className="w-100" />
+          <ScrollableAnchor className="w-100" id={"section1"}>
+            <div className="bg-color page container-fluid">
+              <div className="d-flex display-3 justify-content-center h-75 row align-items-center text-center main-font">
+                <div className="col fade-in gray-font" data-testid="name-intro">
+                  Hey, I'm Brett Cimbalik. I'm a front-end developer.
+                </div>
+              </div>
+              <div className="d-flex display-3 justify-content-center h-25 row  text-center main-font">
+                <div className="col fade-in">
+                  <a className="text-dark" href="#section2">
+                    <i className="fas fa-2x fa-angle-down pulsate-bck" />
+                  </a>
+                </div>
               </div>
             </div>
-            <div className="d-flex display-3 justify-content-center h-25 row  text-center main-font">
-              <div className="col fade-in">
-                <a className="text-dark" href="#section2">
-                  <i className="fas fa-2x fa-angle-down pulsate-bck" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </ScrollableAnchor>
-        <ScrollableAnchor className="w-100" id={"section2"}>
-          <div className="bg-color page container-fluid">
-            <div className="h-50 row align-items-center">
-              <img
-                data-aos="fade-right"
-                className="border-dark img-resp mx-auto rounded-circle"
-                alt="Brett"
-                src="https://res.cloudinary.com/bcimbali/image/upload/dpr_auto,c_scale,f_auto,q_auto:eco/v1534430386/Portfolio/Profile_Pic.jpg"
-              />
-            </div>
-            <div className="d-flex display-3 justify-content-center h-50 row text-center main-font">
-              <div
-                data-aos="fade-left"
-                className="about-font bg-color col gray-font"
-                data-testid="about-text"
-              >
-                I'm a web developer and programmer based in Chicago, IL. <br />
-                <br />I make web applications, usually with the MERN stack. I
-                prefer to work in React/Nextjs, but learning a new language or
-                framework isn't a hassle. I have several years of programming
-                experience and am rather comfortable with front-end or back-end
-                development.
-              </div>
-            </div>
-          </div>
-        </ScrollableAnchor>
-        <ScrollableAnchor id={"section3"}>
-          <div className="bg-color page container-fluid h-100">
-            <div className="d-flex row h-100 align-items-center text-center">
-              {this.state.portfolio_pieces.map((piece) => (
-                <PortfolioCard
-                  key={piece.id}
-                  id={piece.id}
-                  name={piece.name}
-                  image={piece.images[1]}
-                  description={piece.description}
-                  isOpen={this.state.details}
-                  modalHandler={this.modalHandler}
-                  toggle={this.toggle}
+          </ScrollableAnchor>
+          <ScrollableAnchor className="w-100" id={"section2"}>
+            <div className="bg-color page container-fluid">
+              <div className="h-50 row align-items-center">
+                <img
+                  data-aos="fade-right"
+                  className="border-dark img-resp mx-auto rounded-circle"
+                  alt="Brett"
+                  src="https://res.cloudinary.com/bcimbali/image/upload/dpr_auto,c_scale,f_auto,q_auto:eco/v1534430386/Portfolio/Profile_Pic.jpg"
                 />
-              ))}
-              <PortfolioModal
-                isOpen={details}
-                github={selected_piece.github}
-                description={selected_piece.description}
-                images={selected_piece.images}
-                live={selected_piece.live}
-                toggle={this.toggle}
-                name={selected_piece.name}
-              />
-            </div>
-          </div>
-        </ScrollableAnchor>
-        <ScrollableAnchor id={"section4"}>
-          <div className="bg-color page container-fluid">
-            <div className="d-flex display-3 justify-content-center h-100 row align-items-center text-center main-font">
-              <div data-aos="fade-right" className="bg-color">
-                <p className="contact-font gray-font">
-                  Say hello: <br />
+              </div>
+              <div className="d-flex display-3 justify-content-center h-50 row text-center main-font">
+                <div
+                  data-aos="fade-left"
+                  className="about-font bg-color col gray-font"
+                  data-testid="about-text"
+                >
+                  I'm a web developer and programmer based in Chicago, IL.{" "}
                   <br />
-                  hello [at] brettcimbalik [dot] com
-                </p>
+                  <br />I make web applications, usually with the MERN stack. I
+                  prefer to work in React/Nextjs, but learning a new language or
+                  framework isn't a hassle. I have several years of programming
+                  experience and am rather comfortable with front-end or
+                  back-end development.
+                </div>
               </div>
             </div>
-          </div>
-        </ScrollableAnchor>
+          </ScrollableAnchor>
+          <ScrollableAnchor id={"section3"}>
+            <div className="bg-color page container-fluid h-100">
+              <div className="d-flex row h-100 align-items-center text-center">
+                {this.state.portfolio_pieces.map((piece) => (
+                  <PortfolioCard
+                    key={piece.id}
+                    id={piece.id}
+                    name={piece.name}
+                    image={piece.images[1]}
+                    description={piece.description}
+                    isOpen={this.state.details}
+                    modalHandler={this.modalHandler}
+                    toggle={this.toggle}
+                  />
+                ))}
+                <PortfolioModal
+                  isOpen={details}
+                  github={selected_piece.github}
+                  description={selected_piece.description}
+                  images={selected_piece.images}
+                  live={selected_piece.live}
+                  toggle={this.toggle}
+                  name={selected_piece.name}
+                />
+              </div>
+            </div>
+          </ScrollableAnchor>
+          <ScrollableAnchor id={"section4"}>
+            <div className="bg-color page container-fluid">
+              <div className="d-flex display-3 justify-content-center h-100 row align-items-center text-center main-font">
+                <div data-aos="fade-right" className="bg-color">
+                  <p className="contact-font gray-font">
+                    Say hello: <br />
+                    <br />
+                    hello [at] brettcimbalik [dot] com
+                  </p>
+                </div>
+              </div>
+            </div>
+          </ScrollableAnchor>
+        </div>
         <Footer />
-      </div>
+      </>
     );
   }
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,38 +3,40 @@ import React from "react";
 /** Navigation bar for site */
 function Navbar() {
   return (
-    <nav className="bg-color border-bottom border-dark d-flex navbar flex-nowrap gray-font justify-content-between navbar-expand-lg navbar-light fixed-top">
-      <h1>
-        <a
-          className="fade-in hvr-underline-from-center name-font navbar-brand font-weight-bold m-0 px-2 gray-font"
-          data-testid="brett-cimbalik"
-          href="#section1"
-        >
-          Brett Cimbalik
-        </a>
-      </h1>
-      <div className="d-flex justify-content-center ml-auto nav-list-font">
-        <a
-          className="fade-in hvr-underline-from-center px-2 text-dark"
-          data-testid="about"
-          href="#section2"
-        >
-          About
-        </a>
-        <a
-          className="fade-in hvr-underline-from-center px-2 text-dark"
-          data-testid="portfolio"
-          href="#section3"
-        >
-          Portfolio
-        </a>
-        <a
-          className="fade-in hvr-underline-from-center px-2 text-dark"
-          data-testid="contact"
-          href="#section4"
-        >
-          Contact
-        </a>
+    <nav className="bg-color border-bottom border-dark d-flex navbar flex-nowrap gray-font justify-content-center navbar-expand-lg navbar-light fixed-top nav-container">
+      <div className="nav-inner-container">
+        <h1>
+          <a
+            className="fade-in hvr-underline-from-center name-font navbar-brand font-weight-bold m-0 px-2 gray-font"
+            data-testid="brett-cimbalik"
+            href="#section1"
+          >
+            Brett Cimbalik
+          </a>
+        </h1>
+        <div className="d-flex justify-content-center ml-auto nav-list-font">
+          <a
+            className="fade-in hvr-underline-from-center px-2 text-dark"
+            data-testid="about"
+            href="#section2"
+          >
+            About
+          </a>
+          <a
+            className="fade-in hvr-underline-from-center px-2 text-dark"
+            data-testid="portfolio"
+            href="#section3"
+          >
+            Portfolio
+          </a>
+          <a
+            className="fade-in hvr-underline-from-center px-2 text-dark"
+            data-testid="contact"
+            href="#section4"
+          >
+            Contact
+          </a>
+        </div>
       </div>
     </nav>
   );
